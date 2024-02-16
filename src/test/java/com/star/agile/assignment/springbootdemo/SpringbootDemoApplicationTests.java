863285eb49b0981ce66d726f85189a914a003ba2
Winith <vineethuchiha@gmail.com>
2024-02-16 21:53:35 +0530

    Committed Springboot-demo

---
+++ src/test/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
